a978c9279b4207d39f4a7601ff2348de7bb71206
Jaira <jae@wireless-10-104-254-53.umd.edu>
2017-10-24 19:55:20 -0400

    This is assignment 2

--- assignment_2/form_display.php
+++ assignment_2/form_display.php
@@ -6,14 +6,77 @@
 		margin-top: 20px;
 		margin-bottom: 20px;
 	}
+
+	tr:first-child 
+	{ background: red;
+
+	 }
+
+	th
+	{
+		font-family: sans-serif;
+		font-style: italic;
+		background-color:pink;
+		color:black;
+}
+	td{  
+	background-color: powderblue;
+  text-align:center;
+}
+
+
 </style>
 
 
 </head>
 <body>
 
+
 <?php 
 // The code that you recieve input data from the form goes to here.
+
+$firstname = $_POST["firstname"];
+$lastname = $_POST["lastname"];
+$email = $_POST["email"];
+$address = $_POST["address"];
+$city = $_POST["city"];
+
+
+
+echo "<br>";
+echo "<br>";
+echo "<br>";
+echo"<table width='1000' border='1'>"; 
+echo"<tr>";
+echo "<th style='background-color:00FF00;'>" . "Title". "</th>";
+echo "<td>" . "Value" . "</td>";
+echo "<td>" . "Validations" . "</td>";
+echo"<tr>";
+echo "<th>" . "First Name" . "</th>";
+echo "<td>" . $firstname. "</td>";
+echo "<td>" . "exist" . "</td>";
+echo"<tr>";
+echo "<th>" . "Last Name" . "</th>";
+echo "<td>" . $lastname. "</td>";
+echo "<td>" . "new" . "</td>";
+echo"<tr>";
+echo "<th>" . "Email" . "</th>";
+echo "<td>" . $email. "</td>";
+echo "<td>" . "exist" . "</td>";
+echo"<tr>";
+echo "<th>" . "Address" . "</th>";
+echo "<td>" . $address."</td>";
+echo "<td>" . "new" . "</td>";
+echo"<tr>";
+echo "<th>" . "City" . "</th>";
+echo "<td>" . $city. "</td>";
+echo "<td>" . "exist" . "</td>";
+
+
+// Could not figure out how to implement the validation aspect of the assignment.
+// My thoughts were to create an if statement that tests any information put into the form
+// against that information in the database. If the info is = that info in the database, 
+// it would return exist. Else, it would return new
 ?>
 
 
--- assignment_2/form_generator.php
+++ assignment_2/form_generator.php
@@ -17,9 +17,84 @@ function validateForm() {
 </head>
 <body>
 
+</head>
+<body>
+
+
 <?php 
 
 // forms need to be generated here inside the PHP tag.
+$server = "localhost";
+$username = "root";
+$password = "root";
+$db = "sakila";
+
+$firstname = "first_name";
+$lastname = "last_name";
+$email = "email";
+$address = "address";
+$city = "city";
+
+
+
+//Creating Connection 
+$conn = mysqli_connect($server, $username, $password, $db);
+
+$sqlOutput= 'SELECT first_name, last_name, email,address, city
+FROM customer
+JOIN address 
+ON address.address_id = customer.address_id
+JOIN city 
+ON city.city_id = address.city_id
+WHERE last_name = "ANDREWS"
+ORDER BY last_name;';
+
+//Checking connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+} else {
+    echo "Connected successfully";
+}
+
+
+function query_to_db($conn, $sqlOutput){
+    $result = mysqli_query($conn,$sqlOutput);
+
+    if ($result) {   
+        echo "Your query was successful";
+    } else {
+        echo "Error: " . $sqlOutput . "<br>" . mysqli_error($conn);
+    }
+
+} 
+
+$output =mysqli_query($conn, $sqlOutput);
+
+if(mysqli_num_rows($output) >0){  
+while($rows = mysqli_fetch_array($output)){  
+echo "<form action='form_display.php' name ='outputTable' method='POST'>";
+echo "<br>";
+echo "First Name: <input type='text' name='firstname' value=$rows[$firstname]>";
+echo "<br>";
+echo "Last Name: <input type='text' name='lastname' value=$rows[$lastname]>";
+echo "<br>";
+echo "Email: <input type='email' name='email' value=$rows[$email]>";
+echo "<br>";
+echo "Address: <input type='text' name='address' value='$rows[$address]'";
+echo "<br>";
+echo "<br>";
+echo "City: <input type='text' name='city' value=$rows[$city] >";
+echo "<br>";
+
+echo "<input type='submit' value='submit'>";
+echo "</form>";
+} } 
+else { 
+    echo "Zero Results";
+}
+
+mysqli_close($conn);
+
 
 ?>
 
